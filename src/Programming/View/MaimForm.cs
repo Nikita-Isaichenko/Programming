990c8d387c834548196741b190c90def0b3c355e
Nikita <nikita_isaichenk@ro.ru>
2022-03-05 12:14:26 +0700

    .

--- src/Programming/View/MaimForm.cs
+++ src/Programming/View/MaimForm.cs
@@ -66,8 +66,7 @@ namespace Programming.View
 
         private void ParseButton_Click(object sender, EventArgs e)
         {
-            string text = ParsingTextBox.Text;
-            
+            string text = ParsingTextBox.Text;           
             if (Enum.TryParse(text, out Weekday _))
             {
                 WeekdayOutputLabel.Text = $"Это день недели ({text} = {(int)Enum.Parse(typeof(Weekday), text)})";
