7429b07932970f6419d0afb54a5a23f7c0216f9c
Nikita <nikita_isaichenk@ro.ru>
2022-10-15 15:55:56 +0700

    .

--- src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.cs
@@ -1,21 +1,41 @@
-﻿using ObjectOrientedPractics.View.Tabs;
+﻿using ObjectOrientedPractics.Model;
+using ObjectOrientedPractics.Services;
+using ObjectOrientedPractics.View.Tabs;
 using System.Windows.Forms;
 
 
 namespace ObjectOrientedPractics
 {
     public partial class MainForm : Form
-    {      
+    {
+        /// <summary>
+        /// Предоставляет данные в списках.
+        /// </summary>
+        private Store _store;
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="MainForm"/>
+        /// </summary>
         public MainForm()
-        {
-            InitializeComponent();           
+        {           
+            if (Serializer.IsFile("Store"))
+            {
+                _store = Serializer.LoadFromFile("Store");
+            }
+            else
+            {
+                _store = new Store();
+            }
+
+            InitializeComponent();
+
+            MainItemsTab.Items = _store.Items;
+            MainCustomerTab.Customers = _store.Customers;
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            CustomerTab.SaveCustomersData();
-            ItemsTab.SaveItemsData();
-
+            Serializer.SaveToFile("Store", _store);
         }
     }
 }
