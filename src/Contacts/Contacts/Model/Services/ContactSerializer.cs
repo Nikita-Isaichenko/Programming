cbe6965cc7d8d0c215b4f7fca9c39d46e28800e6
Nikita <nikita_isaichenk@ro.ru>
2023-03-08 17:22:37 +0700

    feature(MainWindow): добавлены привязки данных для полей ввода и вывода

--- src/Contacts/Contacts/Model/Services/ContactSerializer.cs
+++ src/Contacts/Contacts/Model/Services/ContactSerializer.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace View.Model.Services
 {
     internal class ContactSerializer
     {
+
+        public string Path { get; set; }
+            = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public ContactSerializer()
+        {
+
+        }
+
+        public Contact? Load(string fileName)
+        {
+            Contact? contact;
+
+            using (StreamReader sr = new StreamReader(Path + fileName))
+            {
+                contact = JsonConvert.DeserializeObject<Contact>(sr.ReadToEnd());
+            }
+
+            return contact;
+        }
+
+        public void Save(string fileName, Contact contact)
+        {
+            using(StreamWriter wr = new StreamWriter(Path + fileName))
+            {
+                wr.Write(JsonConvert.SerializeObject(contact));
+            }
+        }
     }
 }
--- src/Contacts/Contacts/ViewModel/MainVM.cs
+++ src/Contacts/Contacts/ViewModel/MainVM.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using View.Model;
 
 namespace View.ViewModel
 {
-    internal class MainVM
+    internal class MainVM : INotifyPropertyChanged
     {
+        public Contact Contact { get; private set; } = new Contact();
+
+        public string? Name
+        {
+            get => Contact.Name;
+            set
+            {
+                Contact.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string? PhoneNumber
+        {
+            get => Contact.PhoneNumber;
+            set
+            {
+                Contact.PhoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        public string? Email
+        {
+            get => Contact.Email;
+            set
+            {
+                Contact.Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        
     }
 }
